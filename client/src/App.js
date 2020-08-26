20e5e87fb9e8677bc6d6cf6a30fe10396329b943
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-26 16:00:41 -0700

    attempt | get courses from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const names = ["whale", "squid", "turtle", "coral", "starfish"];
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      //communicates to state if a response has failed
+      courses: ["whale", "squid", "turtle", "coral", "starfish"]
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ul>{names.map(name => <li key={name}> {name} </li>)}</ul>
-    </div>
-  );
+  componentDidMount() {
+    fetch('http://localhost:3000/api/courses', {
+      method: 'GET',
+      credentials: 'same-origin',
+      redirect: 'follow',
+      agent: null,
+      headers: {
+          "Content-Type": "text/plain",
+          'Authorization': 'Basic ' + btoa('gino@coolcats.com:password'),
+      },
+      timeout: 5000
+    })
+      .then(res => res.json())
+      .then(courses => this.setState({courses}, () => console.log('courses fetched...',courses)))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <ul>{this.state.courses.map(name => <li key={this.state.courses}> {this.state.courses} </li>)}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
