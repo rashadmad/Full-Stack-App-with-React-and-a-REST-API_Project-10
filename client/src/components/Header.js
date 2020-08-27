10833f140a9da5ec9c44cab000f983015281a7d8
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-27 11:22:38 -0700

    attempt to add stylesheets

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,9 +11,9 @@ export default class Header extends React.PureComponent {
     return (
         <div>
             <header className="header">
-                <div class="bounds">
-                    <h1 class="header--logo">Courses</h1>
-                    <nav><a class="signup" href="sign-up.html">Sign Up</a><a class="signin" href="sign-in.html">Sign In</a></nav>
+                <div className="bounds">
+                    <h1 className="header--logo">Courses</h1>
+                    <nav><a className="signup" href="sign-up.html">Sign Up</a><a className="signin" href="sign-in.html">Sign In</a></nav>
                 </div>
             </header>
         </div>
