6bb1dfc8bfced5a92ce6a209e1face87af78f118
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-27 12:44:12 -0700

    add head to index.js

--- client/src/components/Head.js
+++ client/src/components/Head.js
@@ -6,8 +6,8 @@ export default class Head extends React.PureComponent {
             <head>
                 <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet" type="text/css"></link>
                 <link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet" type="text/css"></link>
-                <link href="../styles/global.css" rel="stylesheet"></link>
-                <link href="../src/App.css" rel="stylesheet"></link>
+                <link href="./src/styles/global.css" rel="stylesheet"></link>
+                <link href="./src/App.css" rel="stylesheet"></link>
                 <title>Courses</title>
             </head>
         )
