93345b67edec84bdefec05938d87590ee3db9107
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-30 09:43:44 -0700

    create course populate function

--- client/src/components/courseDetail.js
+++ client/src/components/courseDetail.js
@@ -16,7 +16,6 @@ export default class CourseDetail extends React.PureComponent {
     this.courseId = null;
   }
   deleteThisCourse() {
-    debugger;
     fetch(`http://localhost:5000/api/courses/${this.courseId}`, {
       method: "DELETE",
       credentials: "same-origin",
@@ -27,28 +26,30 @@ export default class CourseDetail extends React.PureComponent {
         Authorization: "Basic " + btoa("gino@coolcats.com:password"),
       },
       timeout: 5000,
-    });
+    })
+      .then(res => res.json())
+      .then(courses => this.setState({courses}, () => console.log('courses fetched...',courses)))
+  }
+
+  populateThisCourse() {
+    fetch(`http://localhost:5000/api/courses/${this.courseId}`, {
+      method: 'GET',
+      credentials: 'same-origin',
+      redirect: 'follow',
+      agent: null,
+      headers: {
+          "Content-Type": "text/plain",
+          'Authorization': 'Basic ' + btoa('gino@coolcats.com:password'),
+      },
+      timeout: 5000
+    })
+      .then(res => res.json())
+      .then(selectedCourse => this.setState({selectedCourse}, () => console.log('courses fetched...',selectedCourse)))
   }
 
   componentDidMount() {
     this.courseId = this.props.match.params.id;
-    fetch(`http://localhost:5000/api/courses/${this.courseId}`, {
-      method: "GET",
-      credentials: "same-origin",
-      redirect: "follow",
-      agent: null,
-      headers: {
-        "Content-Type": "text/plain",
-        Authorization: "Basic " + btoa("gino@coolcats.com:password"),
-      },
-      timeout: 5000,
-    })
-      .then((res) => res.json())
-      .then((selectedCourse) =>
-        this.setState({ selectedCourse }, () =>
-          console.log("courses fetched...", selectedCourse)
-        )
-      );
+    this.populateThisCourse()
   }
 
   render() {
