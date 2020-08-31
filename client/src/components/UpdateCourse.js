a58829f125992df76fe98daaac426cc7aa72316c
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-31 02:33:56 -0700

    change class to className in update courses page

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -31,26 +31,26 @@ export default class UpdateCourse extends React.PureComponent {
                   <div>
                       <div>
                           <form>
-                              <div class="grid-66">
-                                  <div class="course--header">
-                                      <h4 class="course--label">Course</h4>
-                                      <div><input id="title" name="title" type="text" class="input-title course--title--input" placeholder="Course title..." value="Build a Basic Bookcase"></input></div>
+                              <div className="grid-66">
+                                  <div className="course--header">
+                                      <h4 className="course--label">Course</h4>
+                                      <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." value="Build a Basic Bookcase"></input></div>
                                       <p>By Joe Smith</p>
                                   </div>
-                                  <div class="course--description">
+                                  <div className="course--description">
                                       <div>
-                                          <textarea id="description" name="description" class="" placeholder="Course description...">agasdasdfa</textarea>
+                                          <textarea id="description" name="description" className="" placeholder="Course description...">agasdasdfa</textarea>
                                       </div>
                                   </div>
-                                  <ul class="course--stats--list">
-                                      <li class="course--stats--list--item">
+                                  <ul className="course--stats--list">
+                                      <li className="course--stats--list--item">
                                           <h4>Estimated Time</h4>
-                                          <div><input id="estimatedTime" name="estimatedTime" type="text" class="course--time--input" placeholder="Hours" value="14 hours"></input></div>
+                                          <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" value="14 hours"></input></div>
                                       </li>
-                                      <li class="course--stats--list--item">
+                                      <li className="course--stats--list--item">
                                           <h4>Materials Needed</h4>
                                           <div>
-                                              <textarea id="materialsNeeded" name="materialsNeeded" class="" placeholder="List materials...">
+                                              <textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials...">
                                               * 1/2 x 3/4 inch parting strip
                                               * 1 x 2 common pine
                                               * 1 x 4 common pine
@@ -66,7 +66,7 @@ export default class UpdateCourse extends React.PureComponent {
                                       </li>
                                   </ul>
                               </div>
-                              <div class="grid-100 pad-bottom"><button class="button" type="submit">Update Course</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='course-detail.html';">Cancel</button></div>
+                              <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='course-detail.html';">Cancel</button></div>
                           </form>
                       </div>
                   </div>
