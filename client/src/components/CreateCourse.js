a170dad9cc1aef3e588cfc2fb330f85455495084
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-30 17:51:22 -0700

    attempt to create create course functionality

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 /*
     This component provides the "Create Course" screen by rendering a form that allows a user to create a new course. 
@@ -6,42 +6,110 @@ import React from 'react';
     This component also renders a "Cancel" button that returns the user to the default route (i.e. the list of courses).
 */
 
+/*
+    "title": "Using Sequlize ORM with Express",
+    "description": "In this workshop, you will use the Sequelize ORM and Express to build a simple CRUD application that's connected to a SQL database.",
+    "userId": 1,
+    "estimatedTime": "37-minutes",
+    "materialsNeeded": "Computer Express.js Text-Editor"
+*/
+
+/*
+    "title": "Using Sequlize ORM with Express",
+    "description": "In this workshop, you will use the Sequelize ORM and Express to build a simple CRUD application that's connected to a SQL database.",
+    "userId": 1,
+    "estimatedTime": "37-minutes",
+    "materialsNeeded": "Computer Express.js Text-Editor"
+*/
+
 export default class CreateCourse extends React.PureComponent {
-    render() {
-      return (
-            <div className="bounds course--detail">
-                <h1>Create Course</h1>
-                <div>
-                    <div>
-                        <div>
-                            <h2 className="validation--errors--label">Validation errors</h2>
-                            <div className="validation-errors">
-                                <ul>
-                                    <li>Please provide a value for "Title"</li>
-                                    <li>Please provide a value for "Description"</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <form>
-                            <div className="grid-66">
-                                <div className="course--stats">
-                                    <ul className="course--stats--list">
-                                        <li className="course--stats--list--item">
-                                            <h4>Estimated Time</h4>
-                                            <div><input id="title" name="title" type="text" class="input-title course--title--input" placeholder="Course title..." value=""></input></div>
-                                        </li>
-                                        <li className="course--stats--list--item">
-                                            <h4>Materials Needed</h4>
-                                            <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..."></textarea></div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='/';">Cancel</button></div>
-                        </form>
+  constructor() {
+    super();
+    this.state = {
+        courseTitle: "",
+        courseDescription: "",
+        courseEstimatedTime: "",
+        courseMaterialsNeeded: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.createCourse = this.createCourse.bind(this);
+  }
+
+  handleChange(event) {
+    event.persist();
+    this.setState({courseTitle: event.target.courseTitle});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  createCourse() {
+    fetch(`http://localhost:5000/api/courses/`, { 
+      method: "PUT",
+      credentials: "same-origin",
+      redirect: "follow",
+      agent: null,
+      headers: {
+        "Content-Type": "text/plain",
+        Authorization: "Basic " + btoa("gino@coolcats.com:password"),
+      },
+      timeout: 5000,
+    })
+      .then((res) => res.json())
+      .then((selectedCourse) =>
+        this.setState({ selectedCourse }, () =>
+          console.log("courses fetched...", selectedCourse)
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div className="bounds course--detail">
+        <h1>Create Course</h1>
+        <div>
+          <div>
+            <div>
+              <h2 className="validation--errors--label">Validation errors</h2>
+              <div className="validation-errors">
+                <ul>
+                  <li>Please provide a value for "Title"</li>
+                  <li>Please provide a value for "Description"</li>
+                </ul>
+              </div>
+            </div>
+            <form onSubmit={this.handleSubmit}>
+                <div class="grid-66">
+                    <div class="course--header">
+                        <h4 class="course--label">Course</h4>
+                        <div><input id="title" name="title" type="text" class="input-title course--title--input" placeholder="Course title..." value={this.state.courseTitle} onChange={this.handleChange}></input></div>
+                        <p>By Joe Smith</p>
+                    </div>
+                    <div class="course--description">
+                        <div><textarea id="description" name="description" class="" placeholder="Course description..." value={this.state.courseDescription}></textarea></div>
                     </div>
                 </div>
-            </div>
-        )
-    }
+                <div class="grid-25 grid-right">
+                    <div class="course--stats">
+                        <ul class="course--stats--list">
+                        <li class="course--stats--list--item">
+                            <h4>Estimated Time</h4>
+                            <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" value={this.state.courseEstimatedTime}></input></div>
+                        </li>
+                        <li class="course--stats--list--item">
+                            <h4>Materials Needed</h4>
+                            <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." value={this.state.courseMaterialsNeeded}></textarea></div>
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
