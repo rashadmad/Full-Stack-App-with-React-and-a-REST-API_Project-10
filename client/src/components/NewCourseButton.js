2fa6fc070bb4aa6102669299744401ca6aff6c87
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-31 02:44:33 -0700

    add changes to class to className in NewCourseButton

--- client/src/components/NewCourseButton.js
+++ client/src/components/NewCourseButton.js
@@ -12,7 +12,7 @@ export default class Head extends React.PureComponent {
                 x="0px"
                 y="0px"
                 viewBox="0 0 13 13"
-                class="add"
+                className="add"
               >
                 <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
               </svg>
