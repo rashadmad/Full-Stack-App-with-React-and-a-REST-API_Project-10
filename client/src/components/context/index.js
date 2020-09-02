176a354f18d7cd4f79a15011549780405d4b9178
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-09-01 20:01:14 -0700

    add context component

---
+++ client/src/components/context/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const applicationContext = React.createContext();
+
+export const Provider = applicationContext.Provider;
+export const Consumer = applicationContext.Consumer;
